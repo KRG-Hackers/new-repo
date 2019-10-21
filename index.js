cb25ee8009225f23ba73100d7c3fd33db34c85fd
robbiesanchez <rfsanchez004@gmail.com>
2019-10-21 16:43:54 -0400

    indexjs edited

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
-let team = "krgHackers";
-console.log(team);
-=======
 const K = "Kristina";
 const R = "Robbie";
 const G = "Gil";
 
 console.log(`Welcome to ${K} ${R} ${G} - hackers index.js file!`);
->>>>>>> 19a548b55d911fb54d247f35cc16552ab049e6d8
+
+let team = "Ironhack";
+
+console.log(team);
+
